5d17e28794ad70ba6dfe190326766b558821c661
Suelen.Laffin <suelenlaffin@gmail.com>
2022-08-29 10:20:37 -0300

    .

---
+++ WebApi/WebApi/Controllers/SalgadoController.cs
@@ -0,0 +1,50 @@
+﻿using Data.Model;
+using Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalgadoController : ControllerBase
+    {
+        private SalgadoRepository _repository;
+
+        public SalgadoController()
+        {
+            this._repository = new SalgadoRepository();
+        }
+
+        [HttpGet(Name = "Salgado")]
+        public List<Salgado> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Salgado GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpPost]
+        public string Post(Salgado model)
+        {
+            return _repository.Create(model);
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return _repository.Delete(id);
+        }
+
+        [HttpPut]
+        public string Update(Salgado model)
+        {
+            return _repository.Update(model);
+        }
+
+
+    }
+}
