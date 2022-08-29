9007f2bddc0efa2152e5a0cac652d557517d2ce1
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-08-29 09:26:49 -0300

    Criado o context

---
+++ WebApi/Data/Context/FoodContext.cs
@@ -0,0 +1,16 @@
+﻿using Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context
+{
+    internal class FoodContext : DbContext
+    {
+        public DbSet<Doce> Doces { get; set; }
+        public DbSet<Salgado> Salgados { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+          optionBuilder.UseNpgsql("Host=localhost; Port=5436; Database=postgres; UserId=postgres; Password=123456");
+        }
+    }
+}
---
+++ WebApi/Data/Model/Doce.cs
@@ -0,0 +1,8 @@
+﻿namespace Data.Model
+{
+    public class Doce
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
